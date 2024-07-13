5822940f10c19845ab08124894de385af5c9f91b
Himalaya Singh <161754205+HimalayaSingh3@users.noreply.github.com>
2024-07-13 20:14:51 +0530

    Add files via upload

---
+++ bg changer/index.js
@@ -0,0 +1,40 @@
+let color=document.querySelector("#btn1");
+color.addEventListener("click",() =>{
+        document.querySelector("body").style.backgroundColor="red";
+        document.querySelector('#inner').innerHTML="You Choose Red Color";
+})
+let color1=document.querySelector("#btn2");
+color1.addEventListener("click",() =>{
+        document.querySelector("body").style.backgroundColor="green";
+        document.querySelector('#inner').innerHTML="You Choose Green Color";
+})
+let color2=document.querySelector("#btn3");
+color2.addEventListener("click",() =>{
+        document.querySelector("body").style.backgroundColor="yellow";
+        document.querySelector('#inner').innerHTML="You Choose Yellow Color";
+})
+let color3=document.querySelector("#btn4");
+color3.addEventListener("click",() =>{
+        document.querySelector("body").style.backgroundColor="blue";
+        document.querySelector('#inner').innerHTML="You Choose Blue Color";
+})
+let color4=document.querySelector("#btn5");
+color4.addEventListener("click",() =>{
+        document.querySelector("body").style.backgroundColor="black";
+        document.querySelector('#inner').innerHTML="You Choose Black Color";
+})
+let color5=document.querySelector("#btn6");
+color5.addEventListener("click",() =>{
+        document.querySelector("body").style.backgroundColor="purple";
+        document.querySelector('#inner').innerHTML="You Choose Purple Color";
+})
+let color6=document.querySelector("#btn7");
+color6.addEventListener("click",() =>{
+        document.querySelector("body").style.backgroundColor="pink";
+        document.querySelector('#inner').innerHTML="You Choose Pink Color";
+})
+let color7=document.querySelector("#btn8");
+color7.addEventListener("click",() =>{
+        document.querySelector("body").style.backgroundColor="orange";
+        document.querySelector('#inner').innerHTML="You Choose Orange Color";
+})
